511f332a1d40c058c1a8419a811d9d77974f5cdc
Andrew Purio <52730367+JohnAndrewPurio@users.noreply.github.com>
2021-09-08 22:03:33 +0800

    Add files via upload

---
+++ scenes/uiBuilderReactEvents.js
@@ -0,0 +1,12 @@
+// Please see the uibuilder documentation for the list of available events
+
+export const CONTROL_MESSAGE = 'ctrlMsg'
+export const IO_CONNECTED = 'ioConnected'
+export const ON_MESSAGE = 'msg'
+export const RECEIVED_CONTROL_MESSAGES = 'msgsCtrl'
+export const RECEIVED_MESSAGES_COUNT = 'msgsReceived'
+export const SENT_CONTROL_MESSAGE = 'sentCtrlMsg'
+export const SENT_CONTROL_MESSAGES_COUNT = 'msgsSentCtrl'
+export const SENT_MESSAGE = 'msgSent'
+export const SENT_MESSAGES_COUNT = 'msgsSent'
+export const SERVER_TIME_OFFSET = 'serverTimeOffset'
